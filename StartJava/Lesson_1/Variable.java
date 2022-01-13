4e143389ea0fdb36690aa10df51a981045c79436
Oleg <oleg_yudin@epam.com>
2022-01-13 11:10:45 +0600

    Add homework number one

---
+++ StartJava/Lesson_1/Variable.java
@@ -0,0 +1,23 @@
+class Variable {
+	
+	public static void main(String[] args) {
+
+		byte numberOfKeys = 120;
+		short ramMegabytes = 16_384;
+		int ssdMegabytes = 524_288;
+		long ssdBytes = 549_755_813_888L;
+		float frequencyCpu = 2.4F;
+		double priceLaptopUsd = 2_546.56;
+		char favoriteCharacter = 'F';
+		boolean isLaptop = true;
+
+		System.out.println(numberOfKeys);
+		System.out.println(ramMegabytes);
+		System.out.println(ssdMegabytes);
+		System.out.println(ssdBytes);
+		System.out.println(frequencyCpu);
+		System.out.println(priceLaptopUsd);
+		System.out.println(favoriteCharacter);
+		System.out.println(isLaptop);
+	}
+}
