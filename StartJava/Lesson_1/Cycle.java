1f4a73fcda8913e7a013ef45c9560d3004eceeb8
Oleg <oleg_yudin@epam.com>
2022-01-13 12:30:30 +0600

    Add homework number three

---
+++ StartJava/Lesson_1/Cycle.java
@@ -0,0 +1,29 @@
+class Cycle {
+
+	public static void main(String[] args) {
+		for (int i = 0; i <= 20; i++) {
+			System.out.print(i + " ");
+		}
+
+		System.out.println();
+
+		int i = 6;
+		while (i >= -6) {
+			System.out.print(i + " ");
+			i += -2;
+		}
+
+		System.out.println();
+
+		i = 10;
+		int result = 0;
+		do {
+			if (i % 2 != 0) {
+				result += i;
+			}
+			i++;
+		} while (i <= 20);	
+
+		System.out.print(result);
+	}
+}
