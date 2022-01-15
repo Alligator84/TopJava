b13f72aec6c19af0ae92ab70ebf9bcf322afba40
Oleg <oleg_yudin@epam.com>
2022-01-15 23:57:26 +0600

    Change file Variable.java

--- StartJava/Lesson_1/Variable.java
+++ StartJava/Lesson_1/Variable.java
@@ -1,1 +1,22 @@
-class Variable {        public static void main(String[] args) {        byte numberOfKeys = 120;        short ramMegabytes = 16_384;        int ssdMbyte = 524_288;        long ssdByte = 549_755_813_888L;        float frequencyCpu = 2.4F;        double priceLaptopUsd = 2_546.56;        char favoriteCharacter = 'N';        boolean isLaptop = true;        System.out.println(numberOfKeys);        System.out.println(ramMegabytes);        System.out.println(ssdMbyte);        System.out.println(ssdByte);        System.out.println(frequencyCpu);        System.out.println(priceLaptopUsd);        System.out.println(favoriteCharacter);        System.out.println(isLaptop);    }}
+class Variable {
+    
+    public static void main(String[] args) {
+        byte numberOfKeys = 120;
+        short ramMegabytes = 16_384;
+        int ssdMbyte = 524_288;
+        long ssdByte = 549_755_813_888L;
+        float frequencyCpu = 2.4F;
+        double priceLaptopUsd = 2_546.56;
+        char favoriteCharacter = 'N';
+        boolean isLaptop = true;
+
+        System.out.println(numberOfKeys);
+        System.out.println(ramMegabytes);
+        System.out.println(ssdMbyte);
+        System.out.println(ssdByte);
+        System.out.println(frequencyCpu);
+        System.out.println(priceLaptopUsd);
+        System.out.println(favoriteCharacter);
+        System.out.println(isLaptop);
+    }
+}
