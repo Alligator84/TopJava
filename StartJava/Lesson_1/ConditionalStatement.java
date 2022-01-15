8220d0e978fded9666a970422b9ee38f07191c2c
Oleg <oleg_yudin@epam.com>
2022-01-15 21:46:11 +0600

    Add preferences

--- StartJava/Lesson_1/ConditionalStatement.java
+++ StartJava/Lesson_1/ConditionalStatement.java
@@ -1,32 +1,32 @@
 class ConditionalStatement {
 
-	public static void main(String[] args) {
-		int age = 25;
-		if (age > 20) {
-			System.out.println("This is not teenager");
-		}
+    public static void main(String[] args) {
+        int age = 25;
+        if (age > 20) {
+            System.out.println("This is not teenager");
+        }
 
-		boolean isMale = true;
-		if (isMale) {
-			System.out.println("This is male");
-		}
+        boolean isMale = true;
+        if (isMale) {
+            System.out.println("This is male");
+        }
 
-		if (!isMale) {
-			System.out.println("This is female");
-		}
+        if (!isMale) {
+            System.out.println("This is female");
+        }
 
-		float height = 2.02F;
-		if (height < 1.80) {
-			System.out.println("Average height man");
-		}
+        float height = 2.02F;
+        if (height < 1.80) {
+            System.out.println("Average height man");
+        }
 
-		char firstLetterOfName = 'O';
-		if (firstLetterOfName == 'M') {
-			System.out.println("First letter of name is M");
-		} else if (firstLetterOfName == 'I') {
-			System.out.println("First letter of name is I");
-		} else {
-			System.out.println("First letter of name not M or I");
-		}
-	}
+        char firstLetterOfName = 'O';
+        if (firstLetterOfName == 'M') {
+            System.out.println("First letter of name is M");
+        } else if (firstLetterOfName == 'I') {
+            System.out.println("First letter of name is I");
+        } else {
+            System.out.println("First letter of name not M or I");
+        }
+    }
 }
--- StartJava/Lesson_1/Cycle.java
+++ StartJava/Lesson_1/Cycle.java
@@ -1,28 +1,28 @@
 class Cycle {
 
-	public static void main(String[] args) {
-		for (int i = 0; i <= 20; i++) {
-			System.out.print(i + " ");
-		}
+    public static void main(String[] args) {
+        for (int i = 0; i <= 20; i++) {
+            System.out.print(i + " ");
+        }
 
-		System.out.println();
+        System.out.println();
 
-		int counter = 6;
-		while (counter >= -6) {
-			System.out.print(counter + " ");
-			counter += -2;
-		}
+        int counter = 6;
+        while (counter >= -6) {
+            System.out.print(counter + " ");
+            counter += -2;
+        }
 
-		System.out.println();
+        System.out.println();
 
-		counter = 10;
-		int sumOdd = 0;
-		do {
-			if (counter % 2 != 0) {
-				sumOdd += counter;
-			}
-			counter++;
-		} while (counter <= 20);	
-		System.out.print(sumOdd);
-	}
+        counter = 10;
+        int sumOdd = 0;
+        do {
+            if (counter % 2 != 0) {
+                sumOdd += counter;
+            }
+            counter++;
+        } while (counter <= 20);
+        System.out.print(sumOdd);
+    }
 }
