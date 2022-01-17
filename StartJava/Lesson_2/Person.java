7f32a610b425ce6e3940edfe2d76de3c9cf9534e
Oleg <oleg_yudin@epam.com>
2022-01-17 12:13:49 +0600

    Add new file Person.java

---
+++ StartJava/Lesson_2/Person.java
@@ -0,0 +1,28 @@
+class Person {
+
+     boolean isMale = true;
+     String name = "Oleg";
+     int growth = 202;
+     int weight = 145;
+     int age = 23;
+
+     void walk() {
+        System.out.println("Walking");
+     }
+
+     void sit() {
+        System.out.println("Sitting");
+     }
+
+     void runAway() {
+        System.out.println("Running");
+     }
+
+     void talk() {
+        System.out.println("I say");
+     }
+
+     void teach() {
+        System.out.println("Learning");
+     }
+}
